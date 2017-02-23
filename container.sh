883950b2253b82f9fe778f84e5a2d356f7f0ac61
duruczadam <e12.borocz.adam@palfy-vizugyi.hu>
2017-02-23 09:12:43 +0100

    Update container.sh

--- container.sh
+++ container.sh
@@ -2,5 +2,5 @@
 lxc init ubuntu:16.04 boroczadam
 lxc start boroczadam
 sleep 15
-lxc exec apt-get update --y
-lxc exec apt-get upgrade --y
+lxc exec apt-get update --yes
+lxc exec apt-get upgrade --yes
